3a1617dd38bf5b38c358daacc673b03da74cdeac
asonas <hzw1258@gmail.com>
2013-04-07 18:11:25 +0900

    Add api

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -2,19 +2,22 @@ class IconsController < ApplicationController
   # GET /icons
   # GET /icons.json
   def index
+    respond_to do |format|
+      format.html
+      format.json do
+        icons = Icon.select(:class_name).select(:icon_url).where(class_name: params[:icon_class_names])
+        render json: icons
+      end
+    end
   end
 
   def new
     @icon = Icon.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @icon }
-    end
   end
 
   def create
     @icon = Icon.find_by_class_name params[:icon][:class_name]
+
     if @icon
       @icon.icon_url = params[:icon][:icon_url]
     else
