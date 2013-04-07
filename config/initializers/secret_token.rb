6b89a054eb7b990989ec5f7ec3e401928ecd1204
asonas <hzw1258@gmail.com>
2013-04-07 17:18:33 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatworker::Application.config.secret_token = '1869a729adfeed0a2589953e34d8fb5d7d44bf54d84ca47941402dff83f2ccc91ef7d8cdb566462ef0e0fc3740acae72c475a3c2683b655439df73e688c35664'
